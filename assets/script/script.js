4ba188c83f326bcb1b87e861119347f3f878ddc1
ChidoGang <edgarquintanilla50@yahoo.com>
2021-10-08 00:12:55 -0400

    added to js fixed api link key edited the bug in css

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,12 @@
-$(document).ready(function () {
+
+const weathercard=document.getElementById("Weathercard")
+
+ 
 var cityName = "";
 var lat= "";
 var lon= "";
 var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon +'&exclude=minutely,hourly,alerts&appid=0b2c8dcafe03cc68f1ee010c88b59629'; 
-}) 
+ 
 
 $(".card-deck").empty();
 
@@ -27,3 +30,53 @@ if (uvi <= 2) {
 }
 
 $("#temp").text("Temperature: " + response.current.temp + "")
+$("#humidity").text("humidity: " + response.current.humidity + "%");
+$("#wind").text("wind speed: " + response.current.wind_speed + "MPH");
+$(".color").text(response.current.uvi);
+
+$("#current").css({"display":"block"});
+
+var daily = response.daily;
+
+for (i = 1; i <daily.length -2; i++) {
+
+    var dailyDate = document.createElement("p")
+     dailyDate.innerHTML = moment (daily.dt ,"X").format("MM/DD/YYYY")
+    var dailyTemp = document.createElement("p")
+     dailyTemp.innerHTML = daily[i].temp.day;
+     var dailyHum = document.createElement("p")
+     dailyHum.innerHTML = daily[i].humidity;
+     var dailyIcon =document.createElement("img")
+     dailyIcon.classList="weathericon"
+     $(dailyIcon).attr("src",daily[i].weather[0].icon)
+
+
+    weathercard.appendChild(dailyDate) 
+    weathercard.appendChild(dailyTemp)
+    weathercard.appendChild(dailyHum)
+    weathercard.appendChild(dailyIcon)
+}
+
+function getweather() {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' +cityName + "&langappid=0b2c8dcafe03cc68f1ee010c88b59629";
+}
+
+.then(function (response) {
+lat = response.coord.lat;
+lon = response.coord.lon;
+})
+
+function init(){
+    localStorage.setItem("cityname,repsone.name");
+
+    cityName = localStorage.getItem("cityname");
+    if (cityName !== null) {
+    var cityList=$("button")
+    cityList.addclass("list-group-item-group-item-action");
+    cityList.text(cityName);
+    $("ul").prepend(cityList);   
+    }
+
+    function searchButtion() {
+        cityName = $("input").val(.trim();
+    }
